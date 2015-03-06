be651f51c4921de00acd36ee24230bfc9efb1ca7
Tobin <tobinjbrown@gmail.com>
2015-03-05 20:14:10 -0600

    I AM GROOOOOOOOOOT

---
+++ src/problem_028.py
@@ -0,0 +1,27 @@
+'''
+starting with the number 1 and moving to the right in a 
+clockwise direction a 5 by 5 spiral is formed as follows:
+
+21 22 23 24 25
+20  7  8  9 10
+19  6  1  2 11
+18  5  4  3 12
+17 16 15 14 13
+
+It can be verified that the sum of the numbers on the diagonals is 101.
+What is the sum of the numbers on the diagonals in a 
+1001 by 1001 spiral formed in the same way?
+'''
+
+def spiral_sum(n):
+	total = 1
+	number = 1
+	z = 1
+	while z < n:
+		for x in range(4):
+			number += z+1
+			total += number
+		z += 2
+	return total
+
+print(spiral_sum(1001))
